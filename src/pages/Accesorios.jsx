bdf35e5b6909a12e59aa6d0f2e18c9e94ff08a63
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-04 09:14:45 -0500

    agagem1

--- src/pages/Accesorios.jsx
+++ src/pages/Accesorios.jsx
@@ -1,11 +1,53 @@
-import styles from '../styles/index.module.scss'
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
+import styles from "../styles/index.module.scss";
 
 const Accesorios = () => {
-    return (
-        <>
-            <div className={styles.pack}>patina</div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.pack}>
+        <picture>
+        <Swiper
+          effect={"slide"} // Cambia el efecto a "slide"
+          grabCursor={true}
+          navigation={true} // Habilita la navegación
+          modules={[Navigation]} // Agrega el módulo de navegación
+          autoplay={{ delay: 2000, disableOnInteraction: true }}
+          freeMode={true}
+          className={styles.swiper}
+        >
+          <SwiperSlide>
+            <Image src="/e1.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e3.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e2.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e4.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e5.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e6.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e7.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/e8.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+        </Swiper>
+        </picture>
+      </div>
+    </>
+  );
+};
 
-export default Accesorios
+export default Accesorios;
--- src/pages/Bisuteria.jsx
+++ src/pages/Bisuteria.jsx
@@ -1,9 +1,54 @@
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
 import styles from '../styles/index.module.scss'
 
 const Bisuteria = () => {
     return (
         <>
-            <div className={styles.pack}>skate</div>
+            <div className={styles.pack}>
+            <picture>
+        <Swiper
+          effect={"slide"} // Cambia el efecto a "slide"
+          grabCursor={true}
+          navigation={true} // Habilita la navegación
+          modules={[Navigation]} // Agrega el módulo de navegación
+          autoplay={{ delay: 2000, disableOnInteraction: true }}
+          freeMode={true}
+          className={styles.swiper}
+        >
+          <SwiperSlide>
+            <Image src="/p1.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p2.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p3.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p4.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p5.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p6.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p7.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p8.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/p9.jpg" width={200} height={200} alt="img" />
+          </SwiperSlide>
+        </Swiper>
+        </picture>
+            </div>
         </>
     )
 }
