80e531552af06fb45665c2cbf0753a304dac9e51
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-04 16:32:35 -0500

    agagem3

--- src/pages/Bisuteria.jsx
+++ src/pages/Bisuteria.jsx
@@ -1,56 +1,61 @@
-import Image from 'next/image'
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
-import styles from '../styles/index.module.scss'
+import "swiper/css/effect-coverflow";
+import { Navigation, EffectCoverflow } from "swiper/modules";
+import styles from "../styles/index.module.scss";
 
 const Bisuteria = () => {
-    return (
-        <>
-            <div className={styles.pack}>
-            <picture>
-        <Swiper
-          effect={"slide"} // Cambia el efecto a "slide"
-          grabCursor={true}
-          navigation={true} // Habilita la navegación
-          modules={[Navigation]} // Agrega el módulo de navegación
-          autoplay={{ delay: 2000, disableOnInteraction: true }}
-          freeMode={true}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <Image src="/p1.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p2.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p3.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p4.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p5.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p6.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p7.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p8.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/p9.jpg" width={200} height={200} alt="img" />
-          </SwiperSlide>
-        </Swiper>
-        </picture>
-            </div>
-        </>
-    )
-}
 
-export default Bisuteria
+  let width = 220;
+  let height = 200;
+
+  return (
+    <>
+      <div className={styles.pack}>
+        <picture>
+          <Swiper
+            effect={"coverflow"} // Cambia el efecto a "slide"
+            grabCursor={true}
+            navigation={true} // Habilita la navegación
+            modules={[Navigation, EffectCoverflow]} // Agrega el módulo de navegación
+            autoplay={{ delay: 2000, disableOnInteraction: true }}
+            freeMode={true}
+            className={styles.swiper}
+          >
+            <SwiperSlide>
+              <Image src="/p1.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p2.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p3.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p4.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p5.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p6.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p7.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p8.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="/p9.jpg" width={width} height={height} alt="img" />
+            </SwiperSlide>
+          </Swiper>
+        </picture>
+      </div>
+    </>
+  );
+};
+
+export default Bisuteria;
