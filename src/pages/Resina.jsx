22d1d645bcf7711825948cff4b5d9fe1e63abd7f
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-07 08:25:49 -0500

    agagem12

--- src/pages/Resina.jsx
+++ src/pages/Resina.jsx
@@ -1,57 +1,57 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
 import styles from "../styles/index.module.scss";
 
 const Resina = () => {
-
   let width = 220;
   let height = 200;
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true, // Si deseas flechas de navegación
+  };
+
   return (
     <>
-      <div className={styles.pack}>
-        <picture>
-          <Swiper 
-            grabCursor={true}
-            navigation={true} 
-            modules={[Navigation]} // Agrega el módulo de navegación
-            autoplay={{ delay: 2000, disableOnInteraction: true }}
-            freeMode={true}
-            className={styles.swiper}
-          >
-            <SwiperSlide>
-              <Image src="/p1.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p2.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p3.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p4.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p5.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p6.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p7.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p8.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/p9.jpg" width={width} height={height} alt="img" />
-            </SwiperSlide>
-          </Swiper>
-        </picture>
-      </div>
+      <section  className={styles.pack}>
+        <Slider {...settings} className={styles.slider}>
+          <figure>
+            <Image src="/p1.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p2.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p3.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p4.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p5.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p6.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p7.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p8.jpg" width={width} height={height} alt="img" />
+          </figure>
+          <figure>
+            <Image src="/p9.jpg" width={width} height={height} alt="img" />
+          </figure>
+        </Slider>
+      </section>
     </>
   );
 };
