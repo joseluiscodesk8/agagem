254c41455844d76929c818465a4c16b0c70a9265
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-07 22:28:32 -0500

    agagem14

--- src/pages/Pulsera_set_x3.jsx
+++ src/pages/Pulsera_set_x3.jsx
@@ -1,36 +1,54 @@
-import Image from "next/image";
-import styles from "../styles/index.module.scss";
+// import { useState } from "react";
+// import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import Image from "next/image";
+// import styles from "../styles/index.module.scss";
 
-const PulseraSetx3 = () => {
+// const images = [
+//   '/b1.JPG',
+//   '/p2.jpg',
+//   '/e2.jpg',
+//   // Agrega más imágenes según sea necesario
+// ];
 
-    const width = 220;
-  const height = 200;
-    return (
-        <div className={styles.pack}>
-        <div className={styles.customSlider}>
-          <div className={styles.sliderItem}>
-            <Image src="/p1.jpg" width={width} height={height} alt="img" />
-          </div>
-          <div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div>
-          <div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div>
-          <div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div>
-          <div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div><div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div>
-          <div className={styles.sliderItem}>
-            <Image src="/p2.jpg" width={width} height={height} alt="img" />
-          </div>
-        </div>
-      </div> 
-    )
-}
 
-export default PulseraSetx3;
+// const PulseraSetx3 = () => {
+
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   const nextSlide = () => {
+//     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+//   };
+
+//   const prevSlide = () => {
+//     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+//   };
+
+//   return (
+//     <div className={styles.sliderContainer}>
+//     <button onClick={prevSlide} className={styles.sliderButton}>
+//       Prev
+//     </button>
+//     <div className={styles.slider}>
+//       {images.map((image, index) => (
+//         <div
+//           key={index}
+//           className={`${styles.slide} ${index === currentIndex ? styles.active : ''}`}
+//         >
+//           <Image
+//             src={image}
+//             alt={`Image ${index + 1}`}
+//             width={200}
+//             height={200}
+//             layout="fixed"
+//           />
+//         </div>
+//       ))}
+//     </div>
+//     <button onClick={nextSlide} className={styles.sliderButton}>
+//       Next
+//     </button>
+//   </div>
+//   );
+// }
+
+// export default PulseraSetx3;
--- src/pages/Pulseras_duo.jsx
+++ src/pages/Pulseras_duo.jsx
@@ -1,52 +1,93 @@
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/scss";
-import "swiper/scss/pagination";
-import { Navigation } from "swiper/modules";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import styles from "../styles/index.module.scss";
 
-const Puleras_duo = () => {
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
+
+const images = [
+  "/e1.jpg",
+  "/e2.jpg",
+  "/e3.jpg",
+  "/e4.jpg",
+  "/e5.jpg",
+  "/e6.jpg",
+  "/e7.jpg",
+  "/e8.jpg",
+  "/e9.jpg",
+  
+];
+
+const pulseraDuo = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, images.length, page);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
   return (
     <>
-      <div className={styles.pack}>
-        <picture>
-          <Swiper
-            grabCursor={true}
-            navigation={true} 
-            modules={[Navigation]} 
-            autoplay={{ delay: 2000, disableOnInteraction: true }}
-            freeMode={true}
-            className={styles.swiper}
-          >
-            <SwiperSlide>
-              <Image src="/e1.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e3.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e2.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e4.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e5.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e6.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e7.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Image src="/e8.jpg" width={200} height={200} alt="img" />
-            </SwiperSlide>
-          </Swiper>
-        </picture>
+      <div className={styles.agagem}>
+      <buttons className="next" onClick={() => paginate(1)}>
+        <FiChevronLeft />
+      </buttons>
+      <AnimatePresence initial={false} custom={direction}>
+      <figure>
+       <motion.img
+          key={page}
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+       </figure>
+      </AnimatePresence>
+      <buttons className="prev" onClick={() => paginate(-1)}>
+        <FiChevronRight />
+      </buttons>
       </div>
     </>
   );
 };
 
-export default Puleras_duo;
+export default pulseraDuo;
+
