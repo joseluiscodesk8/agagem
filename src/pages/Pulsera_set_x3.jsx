82accf6e31cee6ddf0c4c0308535dab67646555c
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-07 22:31:48 -0500

    agagem15

--- src/pages/Pulsera_set_x3.jsx
+++ src/pages/Pulsera_set_x3.jsx
@@ -1,54 +1,8 @@
-// import { useState } from "react";
-// import { CSSTransition, TransitionGroup } from 'react-transition-group';
-// import Image from "next/image";
-// import styles from "../styles/index.module.scss";
-
-// const images = [
-//   '/b1.JPG',
-//   '/p2.jpg',
-//   '/e2.jpg',
-//   // Agrega más imágenes según sea necesario
-// ];
 
 
-// const PulseraSetx3 = () => {
+const PulseraSetx3 = () => {
 
-//   const [currentIndex, setCurrentIndex] = useState(0);
+  return <div>skate</div>
+}
 
-//   const nextSlide = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-//   };
-
-//   const prevSlide = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-//   };
-
-//   return (
-//     <div className={styles.sliderContainer}>
-//     <button onClick={prevSlide} className={styles.sliderButton}>
-//       Prev
-//     </button>
-//     <div className={styles.slider}>
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           className={`${styles.slide} ${index === currentIndex ? styles.active : ''}`}
-//         >
-//           <Image
-//             src={image}
-//             alt={`Image ${index + 1}`}
-//             width={200}
-//             height={200}
-//             layout="fixed"
-//           />
-//         </div>
-//       ))}
-//     </div>
-//     <button onClick={nextSlide} className={styles.sliderButton}>
-//       Next
-//     </button>
-//   </div>
-//   );
-// }
-
-// export default PulseraSetx3;
+export default PulseraSetx3;
