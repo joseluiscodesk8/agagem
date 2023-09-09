647d10f1ccf85f6f0be5617bf0b9b9c5494dffc3
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-09-09 09:32:51 -0500

    agagem23

--- src/pages/Resina.jsx
+++ src/pages/Resina.jsx
@@ -1,98 +1,51 @@
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { wrap } from "popmotion";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import styles from "../styles/index.module.scss";
-
-const variants = {
-  enter: {
-    x: 200,
-    y: -200,
-    rotateZ: -45, // Rotación en el eje Z
-    opacity: 0,
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    y: 0,
-    rotateZ: 0,
-    opacity: 1,
-  },
-  exit: {
-    zIndex: 0,
-    x: -200,
-    y: 200,
-    rotateZ: 45,
-    opacity: 0,
-  },
-};
-
-const swipeConfidenceThreshold = 10000;
-const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
+import React, { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-creative';
+import { EffectCreative } from 'swiper/modules';
+import Image from 'next/image';
+import styles from '../styles/index.module.scss'; // Importa tus estilos Sass como un objeto
 
 const images = [
-  "/e1.jpg",
-  "/e2.jpg",
-  "/e3.jpg",
-  "/e4.jpg",
-  "/e5.jpg",
-  "/e6.jpg",
-  "/e7.jpg",
-  "/e8.jpg",
-  "/e9.jpg",
-  
+  '/e1.jpg',
+  '/e2.jpg',
+  '/e3.jpg',
+  '/e4.jpg',
+  '/e5.jpg',
+  // Agrega aquí más rutas de imágenes según sea necesario
 ];
 
-const Resina = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [[page, direction], setPage] = useState([0, 0]);
-  const imageIndex = wrap(0, images.length, page);
-
-  const paginate = (newDirection) => {
-    setPage([page + newDirection, newDirection]);
-  };
+const Resina = () => { // Cambia el nombre de la función a Resina
+  const swiperRef = useRef(null);
 
   return (
     <>
-      <section className={styles.agagem}>
-      <button className="next" onClick={() => paginate(1)}>
-        <FiChevronLeft />
-      </button>
-      <AnimatePresence initial={false} custom={direction}>
-      <figure>
-       <motion.img
-          key={page}
-          src={images[imageIndex]}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 },
-          }}
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }}
-          dragElastic={1}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x);
-
-            if (swipe < -swipeConfidenceThreshold) {
-              paginate(1);
-            } else if (swipe > swipeConfidenceThreshold) {
-              paginate(-1);
-            }
-          }}
-        />
-       </figure>
-      </AnimatePresence>
-      <button className="prev" onClick={() => paginate(-1)}>
-        <FiChevronRight />
-      </button>
-      </section>
+      <Swiper
+        grabCursor={true}
+        effect={'creative'}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+        className={styles.mySwiper} // Aplica los estilos utilizando la variable "styles"
+        ref={swiperRef}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <figure>
+            <Image src={image} alt={`Slide ${index + 1}`} width={500} height={500} />
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
-};
+}
 
-export default Resina;
+export default Resina; // Exporta el componente con el nombre Resina
